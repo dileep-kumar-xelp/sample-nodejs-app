2e6d1ecb6feafbe7ab40171b62cc981a25c9a9d5
Xelp <xelp@14.143.125.162.static-Bangalore.vsnl.net.in>
2019-03-20 12:46:53 +0530

    Text has been changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const myLib = require("./lib");
 app.get("/", (req, res) => {
   const text = myLib.helloWorld();
-  res.send(`${text} from a Node.js app with ci/cd automated in aws!`);
+  res.send(`${text} from a Node.js app with k8s in aws!`);
 });
 app.listen(3000, () => {
   console.log("Server is up on 3000");
